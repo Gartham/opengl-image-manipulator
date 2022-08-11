afb2df8e697b6d4ade0932d29884d7e11b634dba
Gartham <gartham@dusttoash.org>
2022-08-11 13:39:34 -0500

    Update fragment shader to include simple hue shift

--- src/com/gartham/tools/opengl/imagemanipulator/fragmentShader.fs
+++ src/com/gartham/tools/opengl/imagemanipulator/fragmentShader.fs
@@ -5,9 +5,50 @@ out vec4 outputColor;
 in vec3 pos;
 uniform sampler2D tex;
 
+vec3 hsl2rgb( in vec3 c )
+{
+    vec3 rgb = clamp( abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),6.0)-3.0)-1.0, 0.0, 1.0 );
 
+    return c.z + c.y * (rgb-0.5)*(1.0-abs(2.0*c.z-1.0));
+}
+
+vec3 rgb2hsl( in vec3 c ){
+  float h = 0.0;
+	float s = 0.0;
+	float l = 0.0;
+	float r = c.r;
+	float g = c.g;
+	float b = c.b;
+	float cMin = min(r, min(g, b));
+	float cMax = max(r, max(g, b));
+
+	l = ( cMax + cMin ) / 2.0;
+	if ( cMax > cMin ) {
+		float cDelta = cMax - cMin;
+        
+        //s = l < .05 ? cDelta / ( cMax + cMin ) : cDelta / ( 2.0 - ( cMax + cMin ) ); Original
+		s = l < .0 ? cDelta / ( cMax + cMin ) : cDelta / ( 2.0 - ( cMax + cMin ) );
+        
+		if ( r == cMax ) {
+			h = ( g - b ) / cDelta;
+		} else if ( g == cMax ) {
+			h = 2.0 + ( b - r ) / cDelta;
+		} else {
+			h = 4.0 + ( r - g ) / cDelta;
+		}
+
+		if ( h < 0.0) {
+			h += 6.0;
+		}
+		h = h / 6.0;
+	}
+	return vec3( h, s, l );
+}
 
 void main()
 {
 	outputColor = texture(tex, vec2((pos.x + 1) / 2, (pos.y + 1) / 2));
+	outputColor.xyz = rgb2hsl(outputColor.xyz);
+	outputColor.x*=pos.x*pos.y;
+	outputColor.xyz = hsl2rgb(outputColor.xyz);
 }
